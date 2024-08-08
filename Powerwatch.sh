9b9b890a4640ff0cb03e27e9296dff84d014227a
Andreas Horat <a.horat@hisg.ch>
2024-08-08 21:29:20 +0200

    Updated Powerwatch script

--- Powerwatch.sh
+++ Powerwatch.sh
@@ -1,6 +1,5 @@
-PowerWatch.sh                                                                               
 #!/bin/bash
 
 source /home/powermeter/fs_powerwatch_sw/env/bin/activate
-exec python /home/powermeter/fs_powerwatch_sw/fs_powerwatch_GUI.py
+exec python /home/powermeter/fs_powerwatch_sw/FsPowerWatchGui.py
 pause
